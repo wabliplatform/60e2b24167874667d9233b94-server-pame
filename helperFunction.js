6217c879c6f4414d80160c2b1231544e2e78a9eb
wabliplatform <93119230+wabliplatform@users.noreply.github.com>
2022-11-08 13:04:49 +0200

    My commit message

---
+++ helperFunction.js
@@ -0,0 +1,9 @@
+/** This is a comment template
+  * @param {string} paramName
+  * @return {Array} [Product]
+  */
+export function pregetAllproduct(param) {
+  // insert code here
+  const first1 = Product.slice(0, 1);
+  console.log(first1);
+}
